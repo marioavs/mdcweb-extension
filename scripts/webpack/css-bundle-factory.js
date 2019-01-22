5e189924c9b853aec3fbc2b1dd78a00d6185dde5
Mario Velasco <marioavs@gmail.com>
2019-01-21 21:47:19 -0600

    chore: Update dependies versions

--- scripts/webpack/css-bundle-factory.js
+++ scripts/webpack/css-bundle-factory.js
@@ -1,17 +1,24 @@
 /**
- * Copyright 2016 Google Inc. All Rights Reserved.
+ * @license
+ * Copyright 2019 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 /**
@@ -49,7 +56,6 @@ class CssBundleFactory {
   createCustomCss(
     {
       bundleName,
-      mode = 'production',
       chunks,
       chunkGlobConfig: {
         inputDirectory = null,
@@ -76,7 +82,6 @@ class CssBundleFactory {
 
     return {
       name: bundleName,
-      mode,
       entry: chunks,
       output: {
         path: fsDirAbsolutePath,
@@ -156,6 +161,8 @@ class CssBundleFactory {
   }
 
   createCssLoader_(extractTextPlugin) {
+    const getAbsolutePath = (...args) => this.pathResolver_.getAbsolutePath(...args);
+
     return extractTextPlugin.extract({
       use: [
         {
@@ -168,15 +175,16 @@ class CssBundleFactory {
           loader: 'postcss-loader',
           options: {
             sourceMap: true,
-            plugins: () => [this.autoprefixerLib_({grid: false})],
+            plugins: () => [this.autoprefixerLib_()],
           },
         },
         {
           loader: 'sass-loader',
           options: {
             sourceMap: true,
-            includePaths: this.globber_.getAbsolutePaths('packages/*/node_modules')
-              .concat(this.globber_.getAbsolutePaths('node_modules')),
+            includePaths: [getAbsolutePath('/packages/*/node_modules'), getAbsolutePath('node_modules')],
+            implementation: require('dart-sass'),
+            fiber: require('fibers'),
           },
         },
       ],
